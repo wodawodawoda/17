9f928ad12a8199d93ce74939fb86896e5076a38d
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-04-02 00:58:56 +0200

    Add google-auth, TODO: styles and appearance

--- server.js
+++ server.js
@@ -1,37 +1,64 @@
 const express = require('express');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var config = require('./config');
+var googleProfile = {};
 const app = express();
-app.set('view engine', 'pug');
-app.set('views', './views');
 
 app.use(express.static('assets'));
 
-app.use('/store', (req, res, next) => {
-  console.log('Pośrednik');
-  next();
+// PASSPORT
+passport.serializeUser((user, done) => {
+
+  done(null, user);
 });
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+passport.use(new GoogleStrategy({
+    clientID: config.GOOGLE_CLIENT_ID,
+    clientSecret: config.GOOGLE_CLIENT_SECRET,
+    callbackURL: config.CALLBACK_URL
+  },
+  function (accessToken, refreshToken, profile, cb) {
+    googleProfile = {
+      id: profile.id,
+      displayName: profile.displayName
+    };
+    cb(null,profile);
+  }
+));
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// APP ROUTES
 
 app.get('/', (req, res) => {
-  res.sendFile('/index.html');
+  res.render('auth-google', {user: req.user});
 });
 
-app.get('/first-template', (req, res) => {
-  res.render('first-template', {
-    name: "Dynamic pug",
-    header: "Great pug",
-    showPug: true,
-    bark: "HAU",
-    url: "https://m.popkey.co/287d1c/kve1Z_s-200x150.gif?c=popkey-web&p=popkey&i=dogs-animals&l=search&f=.gif"
-  });
-});
+app.get('/logged', (req,res) => {
+  res.send('bangla')
+})
 
-app.get('/auth/google', (req, res) => {
-  res.render('auth-google', {
-    user: {
-      name: "",
-      permit: false
-    }
-  });
-});
+
+
+// PASSPORT ROUTES
+app.get('/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', {
+    successRedirect: '/logged',
+    failureRedirect: '/'
+}));
+
 
 app.get('/inside', (req, res) => {
   res.render('inside')
@@ -49,12 +76,25 @@ app.get('/userform', (req, res) => {
   res.end(JSON.stringify(response));
 });
 
-const server = app.listen(3001, 'localhost', () => {
-  const host = server.address().address;
-  const port = server.address().port;
+const server = app.listen(3000)
 
-  console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
-});
-app.use((req, res, next) => {
 
-})
+
+
+
+
+// function queueTime(customers, n) {
+//   const len = customers.length;
+//   const sum = customers.reduce((sum, val) => sum + val);
+//   let cashs = []
+//   for(let i = 0; i < len; i++) {
+//     return [...cashs, [customers[i]]
+//   }
+//   for(let i = 0; i < sum; i++) {
+//     const change = cashs.map((val) => val.indexOf(0))
+//     if(change !== -1) {cashs.splice(change, 1, customers.shift())}
+//     if(change === -1 && customers.length === 0) {return i}
+//     cashs.map((val) => {val - 1})
+//   }
+//   console.log(cashs)
+// }
