b8e0a7dbe5026f8a2f9956cb054cda6f1118bbcd
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-04-04 04:42:50 +0200

    Fix POST

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const fs = require('fs');
+const bodyParser = require('body-parser');
 
 const app = express();
+let stringifyFile;
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   console.log('Otrzymałem żądanie GET do strony głównej');
@@ -11,16 +15,18 @@ app.get('/', (req, res) => {
 app.get('/getNote', (req, res) => {
   fs.readFile('./menu.json', 'utf-8', (err,data) => {
     if(err) throw err;
+    stringifyFile = data
     res.send(data);
   });
 });
 
 app.post('/updateNote/:note', (req, res) => {
-  const stringifyFile = req.params.note;
-  fs.writeFile('./menu.json', stringifyFile, (err) => {
+  let note = req.params.note
+  let jsonObj = JSON.parse(stringifyFile)
+  jsonObj.note = note
+  fs.writeFile('./menu.json', JSON.stringify(jsonObj, null, 2), (err) => {
     if (err) throw err;
-    console.log('file updated');
-  });
+  })
 })
 
 app.use((req, res, next) => {
