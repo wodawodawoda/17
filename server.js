c9fc1babacc03f0a1f0578cacee043f1ea59ad2d
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-04-04 05:08:33 +0200

    Add POST version

--- server.js
+++ server.js
@@ -22,3 +22,29 @@ const server = app.listen(3000, 'localhost', () => {
   console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
 });
 
+
+
+// Wersja z POST i bodyParser:
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const app = express();
+
+// app.use(bodyParser())
+// app.use(express.static('assets'));
+
+// app.get('/', (req, res) => {
+//   res.sendFile('/index.html');
+// });
+
+// app.post('/', (req, res) => {
+//   res.end(JSON.stringify(req.body));
+// });
+
+// const server = app.listen(3000, 'localhost', () => {
+//   const host = server.address().address;
+//   const port = server.address().port;
+
+//   console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
+// });
+
